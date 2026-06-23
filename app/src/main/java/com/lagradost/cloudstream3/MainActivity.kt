eb98a7f440f11b27d8480cad290dba7e268066c5
Your Name <you@example.com>
2026-06-23 10:56:27 +0000

    fix: restore stripped generic type parameters in MainActivity

--- app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
+++ app/src/main/java/com/lagradost/cloudstream3/MainActivity.kt
@@ -1396,7 +1396,7 @@ class MainActivity : AppCompatActivity(), ColorPickerDialogListener, BiometricCa
         }
 
 
-        fun setUserData(status: Resource<SyncAPI.AbstractSyncStatus>?) {
+        fun setUserData(status: Resource<SyncViewModel.SyncData>?) {
             if (isLocalList) return
             bottomPreviewBinding?.apply {
                 when (status) {
